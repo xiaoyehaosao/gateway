8bc157db6d04f2bdcf635299b87a6eb6b502932f
xyhs <as8872770@126.com>
2019-11-11 23:16:17 +0800

    初始化

---
+++ src/test/java/com/xyhs/b2c/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xyhs.b2c.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
